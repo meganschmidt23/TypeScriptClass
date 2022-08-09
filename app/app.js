ffa10bf7d3c0cdffca7c1b8204581ba33e549a15
Megan Schmidt <schmidtm6@hawkmail.newpaltz.edu>
2022-08-09 13:53:43 -0400

    Install TypeScript and basic app

--- app/app.js
+++ app/app.js
@@ -3,5 +3,4 @@ function startGame() {
     var messagesElement = document.getElementById('messages');
     messagesElement.innerText = 'Welcome to MultiMath! Starting new game . . .';
 }
-
 document.getElementById('startGame').addEventListener('click', startGame);
