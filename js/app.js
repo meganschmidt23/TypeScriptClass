58aaf14889230af0463d4a33034bfdcb1a733e0f
Megan Schmidt <schmidtm6@hawkmail.newpaltz.edu>
2022-08-09 14:43:29 -0400

    let vs const lesson

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
-"use strict";
 function startGame() {
-    //starting new game
+    var playerName = 'Audrey';
+    logPlayer(playerName);
     var messagesElement = document.getElementById('messages');
     messagesElement.innerText = 'Welcome to MultiMath! Starting new game . . .';
+}
+function logPlayer(name) {
+    console.log("New game starting for player: ".concat(name));
 }
 document.getElementById('startGame').addEventListener('click', startGame);
 //# sourceMappingURL=app.js.map
